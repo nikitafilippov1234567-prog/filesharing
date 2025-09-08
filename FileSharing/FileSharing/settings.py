a040f20f56549b9631e30fbcb9ab215303466890
Kaifarik <nikitafilippov1234567@gmail.com>
2025-09-08 19:07:45 +0300

    Update settings.py

--- FileSharing/FileSharing/settings.py
+++ FileSharing/FileSharing/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,3 +149,4 @@ LOGGING = {
         },
     },
 }
+
