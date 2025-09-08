5d1bbf0c2045a834fa3271ff1a2e56192fff34b9
Kaifarik <nikitafilippov1234567@gmail.com>
2025-09-08 13:04:52 +0300

    Update urls.py

--- FileSharing/FileSharing/urls.py
+++ FileSharing/FileSharing/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', include('main.urls')),
 ]
 
-if settings.DEBUG:  # Only in development
+# Serve media files in development
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
